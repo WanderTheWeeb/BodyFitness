88e81c97974c6ef7f01bf3dec95f42a2ad089255
WanderTheWeeb <josefranciscosantesprimo@gmail.com>
2024-12-04 22:33:10 -0600

    some changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,17 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ['Bebas Neue', 'sans-serif'], 
+				sans: ['Bebas Neue', 'sans-serif'],
 			},
 			backgroundImage: {
 				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
 				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-			  },
+			},
 		},
 	},
-	plugins: [],
+	plugins: [require('tailwindcss-animated')],
 }
